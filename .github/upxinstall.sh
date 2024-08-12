679a750187a64e610da3fca7ca3f01c63e9fdd1a
jiqinga <1758812645@qq.com>
2024-08-12 16:27:50 +0800

    ci: 添加upx安装脚本

---
+++ .github/upxinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+cd /tmp
+wget https://github.com/upx/upx/releases/download/v4.2.4/upx-4.2.4-amd64_linux.tar.xz
+tar -xvJf upx-4.2.4-amd64_linux.tar.xz
--- scripts/upx.sh
+++ scripts/upx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 if [[ ! $1 =~ windows_arm_7 && ! $1 =~ windows_arm64 ]]; then
-    upx -9  --lzma "$1"
+    /tmp/upx-4.2.4-amd64_linux/upx -9  --lzma "$1"
 else
     echo "跳过$1"
 fi
