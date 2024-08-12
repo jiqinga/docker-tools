e9eecac8cba9afd56caf1f89709da46b5807bd50
jiqinga <1758812645@qq.com>
2024-08-12 16:38:52 +0800

    ci: upx打包忽略macos

--- scripts/upx.sh
+++ scripts/upx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-if [[ ! $1 =~ windows_arm_7 && ! $1 =~ windows_arm64 ]]; then
+if [[ ! $1 =~ windows_arm_7 && ! $1 =~ windows_arm64 && ! $1 =~ darwin ]]; then
     /tmp/upx-4.2.4-amd64_linux/upx -9  --lzma "$1"
 else
     echo "跳过$1"
